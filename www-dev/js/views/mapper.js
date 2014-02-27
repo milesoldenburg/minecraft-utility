fc4014c5438124018de7730e36c5cdbdd5cd5429
Miles Oldenburg <moldenbu@cisco.com>
2014-02-26 22:07:00 -0500

    Implemented the map room planner.

--- www-dev/js/views/mapper.js
+++ www-dev/js/views/mapper.js
@@ -3,22 +3,112 @@ define(['jquery', 'backbone', 'text!templates/mapper/mapper.html'], function($, 
     var view = Backbone.View.extend({
         'el' : '.content',
         'events' : {
-            'click a.submit' : function(event){
+            'change select[name="zoom-level"]' : function(event){
+                this.mapOptions.zoom = parseInt($(event.currentTarget).val(), 10);
+                this.renderMap();
+            },
+            'keyup input[name="rows"]' : function(event){
+                var newRows = parseInt($(event.currentTarget).val(), 10);
+                
+                if (this.mapOptions.centerRow > newRows) {
+                    this.mapOptions.centerRow = newRows;
+                }
+
+                this.mapOptions.rows = newRows;
+                
+                $('.room table td').removeClass('selected');
+                
+                this.drawMap();
+                this.renderMap();
+            },
+            'keyup input[name="columns"]' : function(event){
+                var newCols = parseInt($(event.currentTarget).val(), 10);
+                
+                if (this.mapOptions.centerCol > newCols) {
+                    this.mapOptions.centerCol = newCols;
+                }
+                
+                this.mapOptions.columns = newCols;
+                
+                $('.room table td').removeClass('selected');
+                
+                this.drawMap();
+                this.renderMap();
+            },
+            'keyup input[name="x"]' : function(event){
+                this.mapOptions.x = parseInt($(event.currentTarget).val(), 10);
+                this.renderMap();
+            },
+            'keyup input[name="z"]' : function(event){
+                this.mapOptions.z = parseInt($(event.currentTarget).val(), 10);
+                this.renderMap();
+            },
+            'click .room table td' : function(event){
                 event.preventDefault();
                 
-                var x = $('input[name="x"]').val();
-                var z = $('input[name="z"]').val();
-                var zoom = $('select[name="zoom-level"]').val();
-                var rows = $('input[name="rows"]').val();
-                var columns = $('input[name="columns"]').val();
+                // Store the cell
+                var cell = $(event.currentTarget);
                 
-                console.log(x, z, zoom, rows, columns);
+                // Add selected class and remove from all others
+                $('.room table td').removeClass('selected');
+                cell.addClass('selected');
                 
+                this.mapOptions.centerCol = parseInt(cell.attr('data-col'), 10);
+                this.mapOptions.centerRow = parseInt(cell.parent().attr('data-row'), 10);
+                
+                this.renderMap();
             }
         },
-        'render' : function() {
+        'render' : function(){
             this.$el.html(MapperTemplate);
+            
+            this.mapOptions = {
+                'rows' : parseInt($('input[name="rows"]').val(), 10),
+                'columns' : parseInt($('input[name="columns"]').val(), 10),
+                'zoom' : parseInt($('select[name="zoom-level"]').val(), 10),
+                'x' : parseInt($('input[name="x"]').val(), 10),
+                'z' : parseInt($('input[name="z"]').val(), 10),
+                'centerCol' : 1,
+                'centerRow' : 1
+            };
+            
+            this.drawMap();
+            this.renderMap();
+            
             return this;
+        },
+        'drawMap' : function(){
+            var table = '';
+            for (var rowIndex = 1; rowIndex <= this.mapOptions.rows; rowIndex++) {
+                table += '<tr data-row="' + rowIndex + '">';
+                
+                for (var colIndex = 1; colIndex <= this.mapOptions.columns; colIndex++) {
+                    table += '<td data-col="' + colIndex + '"';
+                    
+                    if (colIndex == this.mapOptions.centerCol && rowIndex == this.mapOptions.centerRow) {
+                        table += ' class="selected"';
+                    }
+                    
+                    table += '></td>';
+                }
+                
+                table += '</tr>';
+            }
+            
+            $('.room table').html(table);
+        },
+        'renderMap' : function(){
+            var self = this;
+            
+            $('.room table td').each(function(index, element){
+                var col = parseInt($(this).attr('data-col'), 10);
+                var row = parseInt($(this).parent().attr('data-row'), 10);
+                
+                var x = ((self.mapOptions.centerCol - col) * -self.mapOptions.zoom) + self.mapOptions.x;
+                var z = ((self.mapOptions.centerRow - row) * self.mapOptions.zoom) + self.mapOptions.z;
+                
+                $(this).html('[' + x + ', ' + z + ']'); 
+            });
         }
     });
 
